e05c676c3c947568d535dba37b81d8a35a684d3f
Lara Mota <larangmt@gmail.com>
2024-10-05 11:46:25 -0300

    Atualizando credenciais do banco de dados

--- db/conn.js
+++ db/conn.js
@@ -16,15 +16,11 @@ require('dotenv').config(); // Carrega as variáveis de ambiente do .env
 const { Sequelize } = require('sequelize');
 
 // Usando as variáveis de ambiente
-const sequelize = new Sequelize(
-  process.env.DB_NAME,       // Nome do banco de dados
-  process.env.DB_USER,       // Usuário do banco de dados
-  process.env.DB_PASSWORD,   // Senha do banco de dados
-  {
-    host: process.env.DB_HOST,     // Host do banco de dados (remoto ou local)
-    dialect: process.env.DB_DIALECT // Dialeto do banco (MySQL)
-  }
-);
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    port: process.env.DB_PORT || 3306,  
+});
 
 try {
     sequelize.authenticate();
